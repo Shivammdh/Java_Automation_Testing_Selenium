059d36e2afdcf963ee6147b5215225789c5e6307
Shivammdh <67448585+Shivammdh@users.noreply.github.com>
2021-09-29 11:19:13 +0530

    Create DropDown1.java

---
+++ MANAGE DROPDOWN/DropDown1.java
@@ -0,0 +1,50 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+public class Dropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		WebElement staticDropdown=driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+		Select dropdown=new Select(staticDropdown);
+		dropdown.selectByIndex(3);
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		dropdown.selectByVisibleText("AED");
+		System.out.println(dropdown.getFirstSelectedOption().getText());
+		driver.findElement(By.id("divpaxinfo")).click();
+
+		   Thread.sleep(2000L);
+
+		   /*int i=1;
+
+		while(i<5)
+
+		{
+
+		driver.findElement(By.id("hrefIncAdt")).click();//4 times
+
+		i++;
+
+		}*/
+
+		   System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+		for(int i=1;i<5;i++)
+
+		{
+
+		driver.findElement(By.id("hrefIncAdt")).click();
+
+		}
+		
+		
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+
+	}
+
+}
