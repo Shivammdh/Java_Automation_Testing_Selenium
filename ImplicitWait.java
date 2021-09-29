6c594d25b2f81d34da165db185d0b05209b0dafa
Shivammdh <67448585+Shivammdh@users.noreply.github.com>
2021-09-29 11:06:18 +0530

    Create ImplicitWait.java

---
+++ ImplicitWait.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ImplicitWait {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		//here we declear implicit wait in globally
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String[] itemNeeded= {"Brocolli","Cauliflower","Beetroot"};
+		
+		
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+		Thread.sleep(3000);
+		addItems(driver,itemNeeded);
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+		/*hence following driver could't send keys "rahulshettyacademy" in  defined element because hence page takes 
+		some wait for executing this element, so to solve this problem or achive this things in proper way we should 
+		use the concept of implicit Wait, Which is decleared in above line number 16*/ 
+
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'Place Order')]")).click();
+		WebElement dropdownval=driver.findElement(By.cssSelector("select[style='width: 200px;']"));
+		Select Dropdown=new Select(dropdownval);
+		Dropdown.selectByVisibleText("India");
+		driver.findElement(By.cssSelector("input[class='chkAgree']")).click();
+
+
+	}
+
+	public static void addItems(WebDriver driver,String[] itemNeeded)
+	{
+		int j=0;
+		List<WebElement> products=driver.findElements(By.cssSelector("h4.product-name"));
+		for(int i=0;i<products.size();i++)
+		{
+			String[] name=products.get(i).getText().split("-");
+			String formatedName=name[0].trim();
+			List itemNeededList=Arrays.asList(itemNeeded);
+			//Thread.sleep(3000);
+			if(itemNeededList.contains(formatedName))
+			{
+				j++;
+				
+				//click on ADD TO CART
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+				if(j==itemNeeded.length)
+				break;
+			}
+		}
+
+
+
+	}
+
+}
