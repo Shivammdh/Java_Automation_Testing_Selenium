8b525472a149fd44e3afb519470e6a190fbb8bcb
Shivammdh <67448585+Shivammdh@users.noreply.github.com>
2021-10-05 15:52:09 +0530

    Create Frame.java

---
+++ FRAME/Frame.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Frame {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://jqueryui.com/droppable/");
+		
+		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='demo-frame']")));
+		//driver.findElement(By.id("draggable")).click();
+		Actions a=new Actions(driver);
+		WebElement source=driver.findElement(By.id("draggable"));
+		WebElement target=driver.findElement(By.id("droppable"));
+		a.dragAndDrop(source, target).build().perform();
+
+
+	}
+
+}
