a6ff1c21f8249b7740ac595b0f5efe1cb81b3863
Shivammdh <67448585+Shivammdh@users.noreply.github.com>
2021-10-17 20:43:59 +0530

    Create GenricHandleCaleneder.java

---
+++ Calendar Handle/GenricHandleCaleneder.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GenricHandleCaleneder {
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.path2usa.com/travel-companions");
+			driver.findElement(By.id("travel_date")).click();
+			while(!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("March"))
+			{
+				driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+			}
+			List<WebElement> dates=driver.findElements(By.className("day"));
+			int count=driver.findElements(By.className("day")).size();
+			for(int i=0;i<count;i++)
+			{
+				String text=driver.findElements(By.className("day")).get(i).getText();
+				if(text.equalsIgnoreCase("23")) {
+					driver.findElements(By.className("day")).get(i).click();
+					break;
+				}
+			}
+
+		}
+
+
+}
